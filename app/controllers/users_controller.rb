3d9422f82b503c4dc10302674fb2aac9a019f03b
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-03-13 18:49:26 +0000

    completed Authentication System

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,51 @@
 class UsersController < ApplicationController
+  def authenticate
+    #get the username from params
+    #get the password from params
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    # look up the record from the db matching username
+    user = User.where({ :username => un }).at(0)
+
+    # if there's no record, redirect back to sign in form
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name 'round"})
+    else
+      # if there is a record, check to see if password matches
+      if user.authenticate(pw)
+      session.store(:user_id, user.id)#if so, set the cookie
+      #redirect to homepage
+      redirect_to("/", { :notice => "Welcome back, " + user.username + "!"})
+      else 
+        # if not, redirect back to sign in form
+        redirect_to("/user_sign_in", { :alert => "Nice try, sucker!" })
+        #if so, set the cookie
+        #redirect to homepage
+      end
+    end
+
+  end
+
+
+
+
+  def toast_cookies
+    reset_session
+    redirect_to("/", { :notice => "See ya later!"})
+  end
+  
+  def new_session_form
+    render({ :template => "users/signin_form.html.erb" })
+  end
+  
+  
+  def new_registration_form
+
+    render({ :template => "users/signup_form.html.erb" })
+end
+  
+  
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,10 +63,18 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
-
-    user.save
-
-    redirect_to("/users/#{user.username}")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
+    save_status = user.save
+  
+    if save_status == true
+      session.store(:user_id, user.id)
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
+  
+    
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index" })
 
   # User routes
-
+  get("/user_sign_up", { :controller => "users", :action => "new_registration_form"})
+  get("/user_sign_out", { :controller => "users", :action => "toast_cookies"})
   # CREATE
   get("/insert_user_record", {:controller => "users", :action => "create" })
 
   # READ
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:the_username", {:controller => "users", :action => "show"})
+  get("/user_sign_in", { :controller => "users", :action => "new_session_form"})
+
+  post("/verify_credentials", { :controller => "users", :action => "authenticate"})
+
 
   # UPDATE
   get("/update_user/:the_user_id", {:controller => "users", :action => "update" })
