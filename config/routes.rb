c924e71bdb6cad87463625e246091e871bacd304
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-03-14 01:07:51 +0000

    assignment completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
   # User routes
   get("/user_sign_up", { :controller => "users", :action => "new_registration_form"})
   get("/user_sign_out", { :controller => "users", :action => "toast_cookies"})
+  get("/user_sign_in", { :controller => "users", :action => "new_session_form"})
+  post("/verify_credentials", { :controller => "users", :action => "authenticate"})
   # CREATE
-  get("/insert_user_record", {:controller => "users", :action => "create" })
+ post("/insert_user_record", {:controller => "users", :action => "create" })
 
   # READ
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:the_username", {:controller => "users", :action => "show"})
-  get("/user_sign_in", { :controller => "users", :action => "new_session_form"})
-
-  post("/verify_credentials", { :controller => "users", :action => "authenticate"})
+  
 
 
   # UPDATE
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
   # Comment routes
 
   # CREATE
-  get("/insert_comment_record", { :controller => "comments", :action => "create" })
+  post("/insert_comment_record", { :controller => "comments", :action => "create" })
 
   # DELETE
 
